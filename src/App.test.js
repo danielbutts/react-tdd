c69289a4aff4fbf2fca549e3ccd6e80430f340dd
danielbutts <daniel.butts@gmail.com>
2017-08-21 13:17:49 -0700

    added selenium and configured first end to end test.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+/* global it, expect */
+it('App renders without crashing', () => {
+  const component = shallow(<App />);
+  expect(component.exists()).toEqual(true);
 });
